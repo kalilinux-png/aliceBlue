e2765f25427cadc0cf94d7e904f95daeaec5e53b
kalilinux-png <kalilinuxuser5@gmail.com>
2023-09-14 14:08:54 +0530

    FEAT:constant.js

---
+++ src/constant.js
@@ -0,0 +1,37 @@
+// For More Info: https://v2api.aliceblueonline.com/appendix
+
+
+
+// Transaction Types
+export const BUY = "B"
+export const SELL = "S"
+
+// Price Types 
+export const LIMIT = "L"
+export const MARKET = "MKT"
+export const SLL = "SL"
+export const SLM = "SL-M"
+
+// Product Types
+export const MIS = "MIS"
+export const CNC = "CNC"
+export const NRML = "NRML"
+
+// Segment Types
+export const Equity = "CASH"
+export const Derivative = "FO"
+export const Commodity = "COM"
+export const Currency = "CUR"
+export const All = "ALL"
+
+// Exchange
+export const NSE = "nse_cm"
+export const BSE = "bse_cm"
+export const NFO = "nse_fo"
+export const MCX = "mcx_fo"
+export const CDS = "cde_fo"
+export const BFO = "mcx_sx"
+export const BCD = "bcs_fo"
+export const NCO = "nse_com"
+export const BCO = "bse_com"
+
